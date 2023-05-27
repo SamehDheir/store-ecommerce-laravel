0c9c64ca8a3eb2159e9b595371ca122c5778c5c8
Sameh Dheir <98809352+SamehDheir@users.noreply.github.com>
2023-05-27 11:17:45 +0300

    update_admin

---
+++ routes/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['middleware' => 'auth:admin'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+
+Route::group(['middleware' => 'guest:admin'], function () {
+    Route::get('login', [LoginController::class, 'getLogin']);
+    Route::post('login', [LoginController::class, 'Login'])->name('admin.login');
+});
